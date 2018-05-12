898c4bd6ac0132fa5368de68e80801a63a42248d
Ed2525 <eduardos.z199625@gmail.com>
2018-05-11 20:12:43 -0600

    first commit

---
+++ Odyssey-client/src/Connection/connect.java
@@ -0,0 +1,28 @@
+package Connection;
+import java.net.*;
+import java.io.*;
+public class connect {
+    public static void main(String[] args){
+        connect x = new connect();
+        x.initClient();
+    }
+    final String Host="localholst";
+    final int PORT=5000;
+    Socket sc;
+
+    DataOutputStream out;
+    DataInputStream in;
+    public void initClient(){
+        try{
+            sc=new Socket(Host,PORT);
+
+            out=new DataOutputStream(sc.getOutputStream());
+
+            out.writeUTF("holi");
+
+            sc.close();
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+}
